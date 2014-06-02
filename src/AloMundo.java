324a0a3d7096d99fc09af1b167ff277000668abc
spartacusrtt <spartacusrtt@hotmail.com>
2014-06-01 22:57:11 -0300

    segundo commite da noite

--- src/AloMundo.java
+++ src/AloMundo.java
@@ -2,7 +2,12 @@
 public class AloMundo {
 	
 	public static void main(String args[]){
-		System.out.println("spartacus");
+		System.out.println("spartacus sousa da silva");
+		
+		
+
+		}
+		
 	}
 
-}
+
