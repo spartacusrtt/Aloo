4efc7dfde40c8776b3466c1a90128c98360b8e44
spartacusrtt <spartacusrtt@hotmail.com>
2014-06-08 19:02:23 -0300

    comi 3

--- src/AloMundo.java
+++ src/AloMundo.java
@@ -3,11 +3,25 @@ public class AloMundo {
 	
 	public static void main(String args[]){
 		System.out.println("spartacus sousa da silva");
-		
-		
-
 		}
 		
-	}
+		
+		//retirar quantidade
+		public int retirar (int quantidade) {
+		    	int _balance;
+				if (quantidade> _balance);{
+		              return -1;
+				}else {
+		           _balance -= quantidade;		    
+		              return 0;	
+		       }	
+	    	}
+
+}
+		
+
+	
+		
+	
 
 
